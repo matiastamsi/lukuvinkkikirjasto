ecbde889e7b2369e67d56cbf03e780e1ca7bd9d3
Matias Tamsi <matias.tamsi@gmail.com>
2020-11-17 12:22:44 +0200

    Lisätty projektiin paketit domain ja ui. Lisäksi asetettu Main-luokkaan väliaikaiseksi 'Hello World'-tulostus kertomaan ohjelman suorituksesta. Lisätty myös kommentteja sekä muutettu paketin lukuvinkkikirjasto alkukirjain pieneksi.

---
+++ Lukuvinkkikirjasto/src/main/java/lukuvinkkikirjasto/Main.java
@@ -0,0 +1,19 @@
+package lukuvinkkikirjasto;
+
+/**
+ * The Main class of the application that is a mini-project on the
+ * 'Ohjelmistotuotanto'-course.
+ *
+ * @author Lukuvinkkikirjasto-group
+ */
+public class Main {
+
+    /**
+     * The main method that launches the user interface.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
