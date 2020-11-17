e9c72fb06769ee2ea6c8ac97fce804d5badd47c1
Ville Savela <villesavela98@gmail.com>
2020-11-17 19:24:12 +0200

    lukuvinkki-luokka lisätty

---
+++ Lukuvinkkikirjasto/src/main/java/lukuvinkkikirjasto/Lukuvinkki.java
@@ -0,0 +1,18 @@
+
+package lukuvinkkikirjasto;
+
+/**
+ *
+ * @author villsave
+ */
+public class Lukuvinkki {
+    private String otsikko;
+    
+    public Lukuvinkki(String otsikko) {
+       this.otsikko = otsikko;
+    }
+    
+    public String getOtsikko() {
+        return this.otsikko;
+    }
+}
