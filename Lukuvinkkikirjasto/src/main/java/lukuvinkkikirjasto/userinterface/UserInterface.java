42e97cf4fe5ce3d3e4d549406cc6193a6781ffaf
Anna Lehto <anviinamaki@gmail.com>
2020-11-19 08:20:20 +0200

    Lisätty UserInterface-luokan metodiin addToLibrary tarkistus, että käyttäjä ei voi lisätä tyhjää eli otsikotonta vinkkiä.

--- Lukuvinkkikirjasto/src/main/java/lukuvinkkikirjasto/userinterface/UserInterface.java
+++ Lukuvinkkikirjasto/src/main/java/lukuvinkkikirjasto/userinterface/UserInterface.java
@@ -6,21 +6,21 @@ import lukuvinkkikirjasto.Lukuvinkki;
 
 public class UserInterface {
     private InputOutput io;
-    List<Lukuvinkki> list;
+    List<Lukuvinkki> library;
 
     public UserInterface(InputOutput io) {
         this.io = io;
-        this.list = new ArrayList<>();
+        this.library = new ArrayList<>();
     }
 
     public void run() {
-        
         io.print("Alla on lueteltu ohjelman toiminnot ja toimintoja vastaavat numeronäppäimet. Valitse toiminto painamalla sitä vastaavaa numeronäppäintä.");
         io.print("1: Lisää uusi lukuvinkki.");
         io.print("2: Listaa lisäämiesi lukuvinkkien otsikot.");
         io.print("0: Poistu ohjelmasta.");
+        
         Boolean continues = true;
-            
+        
         while (continues) {
             io.print("Valitse toiminto (0 - 2): ");
             int command = Integer.valueOf(io.nextLine());
@@ -36,22 +36,25 @@ public class UserInterface {
                     break;
                 default:
                     io.print("Virheellinen näppäinvalinta. Yritä uudestaan.");
-
             }
         }
-        
     }
 
     private void listItems() {
-        for (int i=0; i<this.list.size(); i++) {
-            this.io.print(this.list.get(i).getOtsikko());
-        }
+        this.library.stream()
+            .map(l -> l.getOtsikko())
+            .forEach(t -> io.print(t));
     }
 
     private void addToLibrary() {
         io.print("Anna lukuvinkin otsikko: ");
         String title = io.nextLine();
-        Lukuvinkki newItem = new Lukuvinkki(title);
-        this.list.add(newItem);
+        if (title.equals("")) {
+            io.print("Otsikossa täytyy olla vähintään yksi kirjain. Yritä uudestaan.");
+        } else {
+            Lukuvinkki newItem = new Lukuvinkki(title);
+            this.library.add(newItem);
+            io.print("Lukuvinkin lisääminen onnistui!");
+        }
     }
 }
