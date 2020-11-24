de3ff81b15b22064cbd977c6925c0f36dc944faa
Henri Sundquist <Henri.Sundquist@rokki.net>
2020-11-24 23:19:57 +0200

    Uusi testi

--- Lukuvinkkikirjasto/src/test/java/lukuvinkkikirjasto/UserInterfaceTest.java
+++ Lukuvinkkikirjasto/src/test/java/lukuvinkkikirjasto/UserInterfaceTest.java
@@ -3,6 +3,7 @@ package lukuvinkkikirjasto;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import lukuvinkkikirjasto.userinterface.StubIO;
 import lukuvinkkikirjasto.userinterface.UserInterface;
 import static org.junit.Assert.assertTrue;
@@ -41,4 +42,16 @@ public class UserInterfaceTest {
         assertTrue(prints.get(prints.size() - 2).equals("t3"));
     }
     
+    @Test
+    public void searchFindsATitle() {
+        ArrayList<String> str = new ArrayList<>(Arrays.asList("u", "TestTitle", "e", "e", "Test", "", "p"));
+        StubIO io = new StubIO(str);
+        
+        UserInterface ui = new UserInterface(io);
+        ui.run();
+        
+        ArrayList<String> prints = io.getOutputs();
+        
+        assertTrue(prints.contains("Löydettiin lukuvinkki!"));
+    }
 }
