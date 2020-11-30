e74057e5db414a91f155447a2fb393a5b1bb721e
karhis <otsopa@gmail.com>
2020-11-30 12:54:17 +0200

    testejä

--- Lukuvinkkikirjasto/src/test/java/lukuvinkkikirjasto/UserInterfaceTest.java
+++ Lukuvinkkikirjasto/src/test/java/lukuvinkkikirjasto/UserInterfaceTest.java
@@ -1,5 +1,6 @@
 package lukuvinkkikirjasto;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,8 +11,11 @@ import lukuvinkkikirjasto.databaseconnection.ConnectionToDatabase;
 import lukuvinkkikirjasto.userinterface.ConsoleIO;
 import lukuvinkkikirjasto.userinterface.StubIO;
 import lukuvinkkikirjasto.userinterface.UserInterface;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -21,17 +25,18 @@ import org.junit.Test;
 public class UserInterfaceTest {
 
     UserInterface ui;
-    ArrayList<String> syotteet;
+    ArrayList<String> syotteet = new ArrayList<>();
     StubIO io;
-    ConnectionToDatabase connection;
-    LukuvinkkiDAO dao;
+    static ConnectionToDatabase connection;
+    static LukuvinkkiDAO dao;
 
-    @Before
-    public void setUp() {
-        connection = new ConnectionToDatabase("jdbc:sqlite:tietokantaTest.db");
+    @BeforeClass
+    public static void setUp() throws SQLException, IOException {
+        connection = new ConnectionToDatabase("jdbc:sqlite:tietokanta.db");
         dao = new LukuvinkkiDAO(connection);
-        dao.initializeDatabase(Paths.get("tietokantaTest.db"));
+        dao.createDatabase();
     }
+
 
     @Test
     public void oneTitleCanBeAdded() throws SQLException {
@@ -39,8 +44,8 @@ public class UserInterfaceTest {
         io = new StubIO(str);
         ui = new UserInterface(io, dao);
         ui.run();
-        ArrayList<String> prints = io.getOutputs();
-        assertTrue(prints.get(prints.size() - 2).equals("testTitle"));
+        ArrayList<String> vastaukset = io.getOutputs();
+        assertTrue(vastaukset.contains("Lukuvinkin lisääminen onnistui!"));
     }
 
     @Test
@@ -49,10 +54,34 @@ public class UserInterfaceTest {
         io = new StubIO(str);
         ui = new UserInterface(io, dao);
         ui.run();
-        ArrayList<String> prints = io.getOutputs();
-        assertTrue(prints.get(prints.size() - 4).equals("t1"));
-        assertTrue(prints.get(prints.size() - 3).equals("t2"));
-        assertTrue(prints.get(prints.size() - 2).equals("t3"));
+        ArrayList<String> vastaukset = io.getOutputs();
+        int i = 0;
+        for (String s : vastaukset) {
+            if (s.equals("Lukuvinkin lisääminen onnistui!")){
+                i++;
+            }
+        }
+        assertEquals(i, 3);
+    }
+
+    @Test
+    public void tagsCanBeAdded() throws SQLException {
+        ArrayList<String> str = new ArrayList<>(Arrays.asList("u", "tagTest", "k", "tag", "tagx", "", "e", "l", "p"));
+        io = new StubIO(str);
+        ui = new UserInterface(io, dao);
+        ui.run();
+        ArrayList<String> vastaukset = io.getOutputs();
+        assertTrue(vastaukset.contains("Lukuvinkin lisääminen onnistui!"));
+    }
+
+    @Test
+    public void deletionTest() throws SQLException {
+        ArrayList<String> str = new ArrayList<>(Arrays.asList("u", "delete", "e", "e", "x", "delete", "", "p"));
+        io = new StubIO(str);
+        ui = new UserInterface(io, dao);
+        ui.run();
+        ArrayList<String> vastaukset = io.getOutputs();
+        assertTrue(vastaukset.contains("Lukuvinkki poistettu!"));
     }
 
     @Test
